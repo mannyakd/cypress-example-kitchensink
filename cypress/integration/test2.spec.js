46af94f0cc28f87189673843e8155bb6901de117
mannyakd <borussiareus8>
2019-07-11 13:11:35 -0500

    1. testing ci

---
+++ cypress/integration/test2.spec.js
@@ -0,0 +1,10 @@
+describe('Kitchen test', function() {
+	it('Selects options', function() {
+		cy.visit('http://localhost:8080');
+
+		cy.contains('get')
+			.click();
+
+		cy.contains('cy.get()')
+	})
+});
